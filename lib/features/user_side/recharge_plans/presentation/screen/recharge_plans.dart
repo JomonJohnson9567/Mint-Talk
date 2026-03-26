4a4f78b0c6fb180a544160b8346822a5a02e6be0
Jomon Johnson <jomonjohnsonkm@gmail.com>
2026-03-26 12:07:53 +0530

    Refactor project structure, remove old auth flow, add new feature modules and UI updates

--- lib/features/user_side/recharge_plans/presentation/screen/recharge_plans.dart
+++ lib/features/user_side/recharge_plans/presentation/screen/recharge_plans.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mint_talk/core/constants/app_texts.dart';
+import 'package:mint_talk/core/theme/color.dart';
 import 'package:mint_talk/core/widgets/primary_app_bar.dart';
 import '../widgets/screen_contents.dart';
 
 class RechargePlans extends StatelessWidget {
   const RechargePlans({super.key});
 
-  static const Color _screenBackground = Color(0xFFF4FAF9);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: _screenBackground,
+      backgroundColor: AppColors.white,
       appBar: CustomAppBar(
         title: AppTexts.rechargePlans,
         automaticallyImplyLeading: true,
